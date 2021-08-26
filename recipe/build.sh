8a3c3ffe62089d06435a8b1d34f122a5781a40c3
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-08-26 21:41:44 +0200

    Fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@ set -ex
 
 export FPM_C_COMPILER="${CC}"
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == 1 ]]; then
-  fpm install --compiler "${FC}" --flags "${LDFLAGS} ${FFLAGS}" --prefix "${PREFIX}"
+  fpm install --compiler "${FC}" --flag "${LDFLAGS} ${FFLAGS}" --prefix "${PREFIX}"
 else
   export FFLAGS="${LDFLAGS} ${FFLAGS}"
   ./install.sh --prefix="${PREFIX}"
