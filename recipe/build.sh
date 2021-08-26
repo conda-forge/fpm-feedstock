8b9ed795e0bd74739c9f3662314b4a252e370846
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-08-26 21:32:00 +0200

    Bootstrap only for native build, use existing version for cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,9 @@
 set -ex
 
 export FPM_C_COMPILER="${CC}"
-fpm install --compiler ${FC} --flags "${LDFLAGS} ${FFLAGS}" --prefix "${PREFIX}"
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == 1 ]]; then
+  fpm install --compiler "${FC}" --flags "${LDFLAGS} ${FFLAGS}" --prefix "${PREFIX}"
+else
+  export FFLAGS="${LDFLAGS} ${FFLAGS}"
+  ./install.sh --prefix="${PREFIX}"
+fi
