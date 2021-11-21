f9a15af839afbbd15171129f0b7c873c3c2b2273
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-11-21 17:04:26 +0100

    Update to version 0.5.0
    MNT: Re-rendered with conda-build 3.21.6, conda-smithy 3.14.3, and conda-forge-pinning 2021.11.20.19.20.32

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 set -ex
 
-export FPM_C_COMPILER="${CC}"
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == 1 ]]; then
-  fpm install --compiler "${FC}" --flag "${LDFLAGS} ${FFLAGS}" --prefix "${PREFIX}"
-else
-  export FFLAGS="${LDFLAGS} ${FFLAGS}"
-  ./install.sh --prefix="${PREFIX}"
-fi
+# First build our bootstrapper version
+bootstrap=build/bootstrap
+mkdir -p $bootstrap
+"${FC_FOR_BUILD:-${FC}}" -J $bootstrap -o $bootstrap/fpm fpm-*.F90
+
+# Set environment variables for fpm
+export FPM_FC="${FC}"
+export FPM_CC="${CC}"
+export FPM_AR="${AR:-ar}"
+export FPM_LDFLAGS="${LDFLAGS}"
+export FPM_FCFLAGS="${FCFLAGS}"
+export FPM_CFLAGS="${CFLAGS}"
+
+# Build actual full fpm version
+$bootstrap/fpm install --prefix "${PREFIX}"
