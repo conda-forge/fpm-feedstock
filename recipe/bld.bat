c643b5e676219e5b41fe69c77843a4e16453046b
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2024-10-16 13:40:26 +0200

    Update Windows build script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,7 @@
-mkdir %LIBRARY_BIN%
-copy fpm.exe %LIBRARY_BIN%
+%FC% -J build\bootstrap -o \build\bootstrap\fpm.exe build\bootstrap\fpm.F90
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+set FPM_FC=%FC%
+set FPM_CC=%CC%
+build\bootstrap\fpm.exe install --prefix %PREFIX%
 if %errorlevel% neq 0 exit /b %errorlevel%
