9b510ad0ed860f84ca63df73bb25464b9af48dfe
Sebastian Ehlert <sehlert@microsoft.com>
2024-11-23 08:34:56 +0100

    Compile fpm on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,17 @@
-mkdir %LIBRARY_BIN%
-copy fpm.exe %LIBRARY_BIN%
-if %errorlevel% neq 0 exit /b %errorlevel%
+@echo on
+setlocal enabledelayedexpansion
+
+set bootstrap=build\bootstrap
+set FPM_FC=%FC%
+set FPM_CC=%CC%
+set FPM_AR=%AR%
+set FPM_LFFLAGS=%LDFLAGS%
+set FPM_FCFLAGS=%FCFLAGS%
+set FPM_CFLAGS=%CFLAGS%
+
+mkdir %bootstrap%
+%FC% -J %bootstrap% -o %bootstrap%\fpm.exe %bootstrap%\fpm.F90
+if %ERRORLEVEL% neq 0 exit 1
+
+%bootstrap%\fpm.exe install --prefix %LIBRARY_PREFIX%
+if %ERRORLEVEL% neq 0 exit 1
